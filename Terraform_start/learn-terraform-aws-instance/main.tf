a22deea17d20c6d6d323fc304cf86a853f2d3ec9
SmVN <vladimirsemchishin240801@gmail.com>
2023-08-08 14:39:15 +0300

    исправил ошибку в файле main.tf (лишняя ковычка)

--- Terraform_start/learn-terraform-aws-instance/main.tf
+++ Terraform_start/learn-terraform-aws-instance/main.tf
@@ -5,16 +5,17 @@ terraform {
       version = "~> 3.0.1"
     }
   }
-
-
 }
+
 
 provider "docker" {}
 
-resource "docker_image"" "nginx" {
+
+resource "docker_image" "nginx" {
   name = "nginx:latest"
   keep_locally = false
 }
+
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_idata  
