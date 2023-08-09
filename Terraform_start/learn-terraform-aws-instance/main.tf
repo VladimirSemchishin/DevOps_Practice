4b219fd124659325f581b85444330fde426bee81
SmVN <vladimirsemchishin240801@gmail.com>
2023-08-09 17:20:01 +0300

    Добавил файл для переменных variables.tf и создал там переменную для имени контейнера container_name с дефолтным (значением по умолчанию) <ExampleNginxContainer>

--- Terraform_start/learn-terraform-aws-instance/main.tf
+++ Terraform_start/learn-terraform-aws-instance/main.tf
@@ -19,7 +19,8 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.name
-  name  = "tutorial"
+  #name  = "tutorial"
+  name = var.container_name
 
   ports {
     internal = 80
