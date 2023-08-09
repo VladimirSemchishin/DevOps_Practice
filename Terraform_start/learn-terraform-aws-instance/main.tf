3486a9e78292bd39975e5f68ee9bf22b33dcbaf2
SmVN <vladimirsemchishin240801@gmail.com>
2023-08-09 17:13:10 +0300

    as

--- Terraform_start/learn-terraform-aws-instance/main.tf
+++ Terraform_start/learn-terraform-aws-instance/main.tf
@@ -1,7 +1,7 @@
 terraform {
-  requaried_providers {
+  required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~> 3.0.1"
     }
   }
@@ -12,14 +12,15 @@ provider "docker" {}
 
 
 resource "docker_image" "nginx" {
-  name = "nginx:latest"
+  name         = "nginx:latest"
   keep_locally = false
 }
 
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_idata  
-  name = "tutorial"
+  image = docker_image.nginx.name
+  name  = "tutorial"
+
   ports {
     internal = 80
     external = 8080
