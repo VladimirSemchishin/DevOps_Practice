ce4a444f9bfb3d205481bada6973a5d08af1593c
SmVN <vladimirsemchishin240801@gmail.com>
2023-08-10 12:55:00 +0300

    добавил подсеть yandex_vpc_subnet.practice-vpc-subnet в которой указал network_id через идентификатор сети с указанием передать параметр id : yandex_vpc_network.practice-vpc.id (как переменную)

--- Terraform_start/main.tf
+++ Terraform_start/main.tf
@@ -16,5 +16,10 @@ provider "yandex" {
 }
 
 resource "yandex_vpc_network" "practice-vpc" { #создаем ресурс yandex_vpc_network то как оно пишется можно узнать только из документации, а затем даем ему имя practice-vpc
-  name = "practice" #уникальный идентификатор ресурса будет yandex_vpc_network.practice-vpс
+  name = "practice" #уникальный идентификатор ресурса будет yandex_vpc_network.practice-vpс а имя самой сети будет practice
+}
+
+resource "yandex_vpc_subnet" "practice-vpc-subnet" {
+  v4_cidr_blocks = ["10.2.0.0/16"]
+  network_id     = "${yandex_vpc_network.practice-vpc.id}" #на прямую (типо enpqs3mggbcvk7ibuqga) лучше не передавать идентификатор сети, к которой пренадлежит данная подсеть, yandex_vpc_network.practice-vpс тип ресурса и его назначение через точку - инд. идентиф. на который можно ссылаться как на переменную. Точка id (.id) указывает на то, какой параметр нужно передать.
 }
