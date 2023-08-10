91311ff4ff0943fe7bcbd45a20a3100550fd6a8d
SmVN <vladimirsemchishin240801@gmail.com>
2023-08-10 15:33:10 +0300

    открыт публичный ip и выведены выходные данные через переменные external_ip и external_ip2 выдают они одно и тоже но ссылаются разными путями через значение переменной external_ip в блоке external_ip и через путь от указания такого ресурса как yandex_compute_instance к переменной nat_ip_address блока network_interface, в обоих слуяаях обязательно нужно указать элемент массива, тк ip адрессов на ВМ может быть несколько

--- Terraform_start/init.sh
+++ Terraform_start/init.sh
@@ -2,6 +2,10 @@
 
 ### Install Docker
 
-apt-get update && apt-get install -y apt-transport-https ca-sertificates curl software-properties-common
+apt-get update && apt-get install -y apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 echo "<h2>СОЗДАЛ САЙТ<h2>" > /var/www/html/index.html
 sudo service nginx start 
--- Terraform_start/main.tf
+++ Terraform_start/main.tf
@@ -16,12 +16,20 @@ provider "yandex" {
 }
 
 resource "yandex_vpc_network" "practice-vpc" { #создаем ресурс yandex_vpc_network то как оно пишется можно узнать только из документации, а затем даем ему имя practice-vpc
-  name = "practice" #уникальный идентификатор ресурса будет yandex_vpc_network.practice-vpс а имя самой сети будет practice
+  name = "practice"                            #уникальный идентификатор ресурса будет yandex_vpc_network.practice-vpс а имя самой сети будет practice
 }
 
 resource "yandex_vpc_subnet" "practice-vpc-subnet" {
   v4_cidr_blocks = ["10.2.0.0/16"]
-  network_id     = "${yandex_vpc_network.practice-vpc.id}" #на прямую (типо enpqs3mggbcvk7ibuqga) лучше не передавать идентификатор сети, к которой пренадлежит данная подсеть, yandex_vpc_network.practice-vpс тип ресурса и его назначение через точку - инд. идентиф. на который можно ссылаться как на переменную. Точка id (.id) указывает на то, какой параметр нужно передать.
+  network_id     = yandex_vpc_network.practice-vpc.id #на прямую (типо enpqs3mggbcvk7ibuqga) лучше не передавать идентификатор сети, к которой пренадлежит данная подсеть, yandex_vpc_network.practice-vpс тип ресурса и его назначение через точку - инд. идентиф. на который можно ссылаться как на переменную. Точка id (.id) указывает на то, какой параметр нужно передать.
+}
+
+resource "yandex_vpc_address" "const-ip-vm1" { #для установки постоянного адреса ВМ-1
+  name = "IpAddressVM1"
+
+  external_ipv4_address {
+    zone_id = "ru-central1-a"
+  }
 }
 
 resource "yandex_compute_instance" "vm-1" {
@@ -40,12 +48,21 @@ resource "yandex_compute_instance" "vm-1" {
   }
 
   network_interface {
-    subnet_id = "${yandex_vpc_subnet.practice-vpc-subnet.id}"
-    nat = true
+    subnet_id      = yandex_vpc_subnet.practice-vpc-subnet.id
+    nat            = true
+    nat_ip_address = yandex_vpc_address.const-ip-vm1.external_ipv4_address.0.address #параметр address (в принципе вся строка была написана) был указан после запуска команды terraform plan, в его выводе можно посмотерть вообще все что он меняет и не меняет в плане атрибутов, та же был и адресс (known after apply). Но их может быть несколько, по этому указываем что нужно взять первый элемент массива (.0.address)  
   }
 
-  metadata = {  #все что пишется в этом блоке выполнеяется только после инициации ВМ
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  metadata = { #все что пишется в этом блоке выполнеяется только после инициации ВМ
+    ssh-keys  = "debian:${file("~/.ssh/id_rsa.pub")}"
     user-data = "${file("init.sh")}" #с помощью этого параметра можно передавать например скрипт на исполнение
   }
+}
+
+output "external_ip" { #первый вариант задать выходное значение через конечный идентификатор, который задается как значение переменной network_interface в блоке   network_interface
+  value = yandex_vpc_address.const-ip-vm1.external_ipv4_address.0.address
+}
+
+output "external_ip2" { #в названии можно писать все что угодно, через output в терраформе указываются выходные данные. Тут должен быть тот же вывод что и в прошлои output но значение задается идентификатором переменной в которую клали занчение (  network_interface) то есть идентификатор nat_ip_address (похоже на путь)
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
