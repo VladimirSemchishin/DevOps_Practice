eac27f8fe859b02175e4a3ba297443ac471249d4
SmVN <vladimirsemchishin240801@gmail.com>
2023-08-15 00:56:28 +0300

    переписался файл node_groups, то есть ввелась переменная node_groups и в ней были заданы характеристики для каждой из 3 групп (получилось что в списке node_groups еще 3 списка: node_group-a, node_group-b и node_group-c, в которых описываются группы ключ значение). Таким образом для обращения к списку из 3 элементов (названий групп) объявляется for_each = var.node_groups, обратиться к имени конкретной группы (т.е элементу объявленноо списка можно name        = each.key), чтобы обратиться к характеристикам конкретной группы, необходимо заглянуть в список внутри списка, для это нужно использовать функцию lookup, например чтобы дотянуться до значения кол-ва памяти у группы: cores  = lookup(each.value, cores, 2) говорим выровняй все значения (не ключи) each, те в которых присутсвует cores, возьми то значение на которое оно указывает, если его нет то по дефолту 2. Так описывается практически все в этом файле. Далее полика масштабирования, в ней описаны 2 варианта fixed и auto (если честно я не выкупил зачем и почему, по этому не буду задерживаться)

--- Terraform_start/learn-terraform-yandex-cloud-bestpractices/k8s/locals.tf
+++ Terraform_start/learn-terraform-yandex-cloud-bestpractices/k8s/locals.tf
@@ -1,3 +1,4 @@
 locals {
   network_output = data.terraform_remote_state.network.outputs #теперь если к примеру нужно исп. id сети : local.network_output.network_id
+  worker_subnet_list = zipmap([for subnet in local.network_output.k8s_workers_subnet_info : subnet.zone], [for subnet in local.network_output.k8s_workers_subnet_info : subnet.subnet_id]) #преобразование данных через locals, создается список где каждой зоне присвоится id
 }
--- Terraform_start/learn-terraform-yandex-cloud-bestpractices/k8s/node_group.tf
+++ Terraform_start/learn-terraform-yandex-cloud-bestpractices/k8s/node_group.tf
@@ -1,58 +1,75 @@
 
 #создание группы узлов в кластере k8s
-resource "yandex_kubernetes_node_group" "my_node_group_a" {
+#resource "yandex_kubernetes_node_group" "my_node_group_a" {
+resource "yandex_kubernetes_node_group" "my_node_groups" {   #меняем имя ресурса
+  for_each = var.node_groups
   cluster_id  = yandex_kubernetes_cluster.k8s-regional.id #ссылаемся на кластер k8s
-  name        = "worker-a"                                #имя для всей группы (не одной вм)
-  description = "description"
-  version     = local.k8s_version #указывают версию такую как у мастер ноды
-
-  labels = {
-    "key" = "value"
-  }
+  name        = each.key                                #имя для всей группы (не одной вм)
+  description = lookup(each.value, "description", null)
+  #version     = local.k8s_version #указывают версию такую как у мастер ноды
+  version = lookup(each.value, "version", var.master_version)
+  labels = lookup(each.value, "labels", null)
 
   instance_template {
-    platform_id = "standard-v1"                                                            #поставим 1 вместо 2 чтобы это не значило
-    name        = "worker-a-{instance.short_id}" #задается имя конкретного instace (вм). Поскольку включен futo_scaling ноды будут создаться автоматически, при повышении нагрузки, а так же уничтожаться. По этому фиксированное значение не подойдет. В соотв с докой сделаем через переменную уникальной. В данном случае отметим уникальный id в рамках группы и id группы
+    platform_id = lookup(each.value, "platform_id", null)                                                            #поставим 1 вместо 2 чтобы это не значило
+    #name        = "worker-a-{instance.short_id}" #задается имя конкретного instace (вм). Поскольку включен futo_scaling ноды будут создаться автоматически, при повышении нагрузки, а так же уничтожаться. По этому фиксированное значение не подойдет. В соотв с докой сделаем через переменную уникальной. В данном случае отметим уникальный id в рамках группы и id группы
+    name = lookup(each.value, "name", null)
 
     network_interface {
-      nat                = true
-      subnet_ids        = [dterraform_remote_state.network.mysubnet-a.id] # указал верную существующую подстеть
-      security_group_ids = [syandex_vpc_security_group.k8s-main-sg.id] # продублировал идентификаторы группы безопасности
+      nat                = lookup(each.value, "nat", true)
+      subnet_ids        = [lookup(local.worker_subnet_list, each.value["zone"])]
+      security_group_ids = [lookup(local.network_output.sg_internal, local.network_output.sg_k8s_worker)] 
     }
-
     resources {
-      memory = 2
-      cores  = 2
+      memory = lookup(each.value, "memory", 2)
+      cores  = lookup(each.value, "cores", 2)
     }
-
     boot_disk {
-      type = "network-hdd"
-      size = 32
+      type = lookup(each.value, "boot_disk_type", "network-hdd")
+      size = lookup(each.value, "boot_disk_size", 32)
     }
-
     scheduling_policy {
-      preemptible = false
+      preemptible = lookup(each.value, "preemptible", false)
     }
-
-
   }
 
   scale_policy {
-    auto_scale {
-      min     = 1
-      max     = 4
-      initial = 1
+    dynamic "fixed_scale" {
+      for_each = flatten([lookup(each.value, "fixed_scale", can(each.value["auto_scale"]) ? [] : [{size = 1}])])
+
+      content {
+        size = fixed_scale.value.size
+      }
     }
+
+    dynamic "auto_scale" {
+      for_each = flatten([lookup(each.value, "auto_scale", [])])
+
+      content {
+        min     = auto_scale.value.min
+        max     = auto_scale.value.max
+        initial = auto_scale.value.initial
+      }
+    }
+    #auto_scale {
+    #  min     = 1
+    #  max     = 4
+    #  initial = 1
+    #}
   }
 
   allocation_policy {
     location {
-      zone = "ru-central1-a"
+      #zone = "ru-central1-a"
+      zone = each.value["zone"]
     }
   }
 
 }
 
+
+#Нет необходимости описывать каждую группу, потому что выше описаны все через обращение к значениям через цикл
+/*
 #создание группы узлов в кластере k8s
 resource "yandex_kubernetes_node_group" "my_node_group_b" {
   cluster_id  = yandex_kubernetes_cluster.k8s-regional.id #ссылаемся на кластер k8s
@@ -154,3 +171,4 @@ resource "yandex_kubernetes_node_group" "my_node_group_c" {
     }
   }
 }
+*/
