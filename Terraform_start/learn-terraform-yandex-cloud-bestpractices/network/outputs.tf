5f8418b9372edcf52f6ddb7c6ddf33154ed3aa71
SmVN <vladimirsemchishin240801@gmail.com>
2023-08-14 17:38:54 +0300

    создал output для подсетей мастера и воркера, с ними есть оссобенность поскольку они создавались через цикл, по этому для вывода небходимо условие, а именно 2 атрибутами k и v фильтруется значение переменной var.subnets (она задана списком, в первом случае просматривается только k8s_master,) при помощи ф-ии zipmap создается список где присваивается соответвующее значение ключам subnet_id и zone для каждой переменной (для кажой из 3 подсетей мастера)

--- Terraform_start/learn-terraform-yandex-cloud-bestpractices/network/outputs.tf
+++ Terraform_start/learn-terraform-yandex-cloud-bestpractices/network/outputs.tf
@@ -4,10 +4,12 @@
 #ресурс из network потребуется постоянно указывать конструкцию 
 #data.terraform_remote_state.network.outputs.[имя того output'a который нужен]
 
+
 #==================== network ====================
 output "network_id" {
   value = yandex_vpc_network.network-main.id
 }
+
 
 #==================== security_group ====================
 output "sg_internal" {
@@ -20,4 +22,17 @@ output "sg_k8s_master" {
 
 output "sg_k8s_worker" {
   value = yandex_vpc_security_group.k8s_worker.id
+}
+
+
+#==================== zone and id_subnetwork ====================
+#здесь просто так вывести значение не получится, тк зона и id 
+#подсети задаются циклом, по этому необходимо прописать условие
+
+output "k8s_master_subnet_info" {
+  value = [for k, v in var.var.subnets["k8s_masters"] : zipmap(["subnet_id", "zone"], [yandex_vpc_subnet.mysubnet-main[v.name].id, yandex_vpc_subnet.mysubnet-main[v.name].zone])]
+}
+
+output "k8s_worker_subnet_info" {
+  value = [for k, v in var.var.subnets["k8s_worker"] : zipmap(["subnet_id", "zone"], [yandex_vpc_subnet.mysubnet-main[v.name].id, yandex_vpc_subnet.mysubnet-main[v.name].zone])]
 }
