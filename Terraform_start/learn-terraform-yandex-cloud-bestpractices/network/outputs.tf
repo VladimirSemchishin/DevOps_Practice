23067bef266f161dc44527383e0e34ed209913da
SmVN <vladimirsemchishin240801@gmail.com>
2023-08-14 17:24:27 +0300

    создал output для security_group

--- Terraform_start/learn-terraform-yandex-cloud-bestpractices/network/outputs.tf
+++ Terraform_start/learn-terraform-yandex-cloud-bestpractices/network/outputs.tf
@@ -4,6 +4,20 @@
 #ресурс из network потребуется постоянно указывать конструкцию 
 #data.terraform_remote_state.network.outputs.[имя того output'a который нужен]
 
+#==================== network ====================
 output "network_id" {
   value = yandex_vpc_network.network-main.id
+}
+
+#==================== security_group ====================
+output "sg_internal" {
+  value = yandex_vpc_security_group.internal.id
+}
+
+output "sg_k8s_master" {
+  value = yandex_vpc_security_group.k8s_master.id
+}
+
+output "sg_k8s_worker" {
+  value = yandex_vpc_security_group.k8s_worker.id
 }
