61912f9428960b240c623741d140c6df32fc549d
SmVN <vladimirsemchishin240801@gmail.com>
2023-08-14 17:04:24 +0300

    после terraform apply в каталоге network был записан файл состояния, в том числе и выходные данные, на которые теперь можно ссылаться из других каталогов, например из файла  cluster.tf каталога k8s для описания id сети (id сети это пока единственный output прописанный в network/outputs.tf)

--- Terraform_start/learn-terraform-yandex-cloud-bestpractices/network/outputs.tf
+++ Terraform_start/learn-terraform-yandex-cloud-bestpractices/network/outputs.tf
@@ -1,6 +1,9 @@
 #файл описывающий данные которые будут выводится
+#После запуска terraform apply, данные запишутся 
+#в файл состояния  и стобы сослаться на какой либо 
+#ресурс из network потребуется постоянно указывать конструкцию 
+#data.terraform_remote_state.network.outputs.[имя того output'a который нужен]
 
 output "network_id" {
   value = yandex_vpc_network.network-main.id
 }
-
